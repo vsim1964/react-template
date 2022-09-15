ba8411b3f78fd992bc3518fd95143ebb47e81690
Siminenko <vsim1964@gmail.com>
2022-09-15 13:03:55 +0300

    1 урок

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './index.scss'
+import Module from './module'
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<Module />);
---
+++ src/module.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './img/logo.png'
+
+const Module = () => (
+	<>
+		<h1>Hello, React</h1>
+		<img src={logo} width={200} />
+		);
+	</>
+);
+
+export default Module;
